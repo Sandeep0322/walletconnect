da1cd914d02039207bc5bf9b0738f999cdd1ad24
Sandeep0322 <apple@Apples-MacBook-Pro-21.local>
2023-07-24 15:41:34 +0530

    added wallet connect version 2

---
+++ README.md
@@ -0,0 +1,34 @@
+# React dApp (with standalone v2 client)
+
+Pitaka WalletConnect v2 Docs 
+- https://docs.pitaka.io
+- https://docs.walletconnect.com/2.0
+
+## Overview
+
+This is an example implementation of a React dApp (generated via `create-react-app`) using the standalone
+client for WalletConnect v2 to:
+
+- Connect Pitaka using WalletConnect V2
+
+## Prerequisites
+
+Install the app's dependencies:
+
+```bash
+yarn
+```
+
+Set up your local environment variables by copying the example into your own `.env.local` file:
+
+```bash
+Change: projectId: "<YOUR PROJECT_ID>"
+```
+
+Get Project ID From WalletConnect
+
+## Run Locally
+
+```bash
+yarn start
+```
---
+++ src/components/WagmiConnect.tsx
@@ -0,0 +1,27 @@
+import { useConnect } from 'wagmi'
+import Button from './Button'
+ 
+export function WagmiConnect() {
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect()
+ 
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+          {!connector.ready && ' (unsupported)'}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            ' (connecting)'}
+        </button>
+      ))}
+ 
+      {error && <div>{error.message}</div>}
+    </div>
+  )
+}
